174c4b4bd0e149053bec0cd4244b715e018d128e
omercelikceng <omercelik.ceng@gmail.com>
2023-08-09 10:38:40 -0700

    Initial Commit

---
+++ src/main/java/org/example/graalvm/spring/cloud/GraalVMInitializationApplication.java
@@ -0,0 +1,12 @@
+package org.example.graalvm.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalVMInitializationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraalVMInitializationApplication.class, args);
+    }
+}
